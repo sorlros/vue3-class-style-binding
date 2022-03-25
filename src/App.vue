a2fd80e51e410e69bc89134e19464d5d00f27960
sorlros <sorlros2047@gmail.com>
2022-03-25 15:47:51 +0900

    vue3-getter/setter

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="add">ADD</button>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
@@ -9,19 +10,31 @@
 export default {
   data() {
     return {
+      // Getter, Setter
       msg: "Hello Computed"
     }
   },
   computed: { // computed 에 만들어놓은 계산된 데이터는 캐싱을 통해 한번 연산을 한 값이 있으면 또 다시 연산하지 않는다
-    reversedMessage() {
-      return this.msg.split('').reverse().join('')
+  // Getter (읽기전용)
+    // reversedMessage() {
+    //   return this.msg.split('').reverse().join('')
+    // }
+    // Getter, Setter
+    reversedMessage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      },
+      set(value) {
+        this.msg = value
+        console.log(value)
+      }
+    }
+  },
+  methods: {
+    add() {
+      this.reversedMessage += '!?'
     }
   }
-  // },
-  // methods: {
-  //   reverseMessage() {
-  //     return this.msg.split('').reverse().join('')
-  //   }
-  // }
+  
 }
 </script>
