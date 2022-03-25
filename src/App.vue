4dc6a3d6432f065331c5e27f018389383a4311d8
sorlros <sorlros2047@gmail.com>
2022-03-25 15:59:17 +0900

    vue3-watch

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <button @click="add">ADD</button>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
+  <h1 @click="changeMessage">{{ msg }}</h1>
   <h1>{{ reversedMessage }}</h1>
 </template>
 
@@ -10,31 +7,26 @@
 export default {
   data() {
     return {
-      // Getter, Setter
-      msg: "Hello Computed"
+      msg: 'Hello?'
     }
   },
-  computed: { // computed 에 만들어놓은 계산된 데이터는 캐싱을 통해 한번 연산을 한 값이 있으면 또 다시 연산하지 않는다
-  // Getter (읽기전용)
-    // reversedMessage() {
-    //   return this.msg.split('').reverse().join('')
-    // }
-    // Getter, Setter
-    reversedMessage: {
-      get() {
-        return this.msg.split('').reverse().join('')
-      },
-      set(value) {
-        this.msg = value
-        console.log(value)
-      }
+  computed: {
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  watch: { // 데이터의 변경사항을 감시, 감지가 되면 이후의 로직 실행 // 새로운 매개변수를 할당 가능
+    msg(newValue) {
+      console.log('msg:', newValue)
+    },
+    reversedMessage() {
+      console.log('reversedMessage:', this.reversedMessage)
     }
   },
   methods: {
-    add() {
-      this.reversedMessage += '!?'
+    changeMessage() {
+      this.msg = 'Good!'
     }
   }
-  
 }
 </script>
