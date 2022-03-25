3dc3c5a0e74141c350ca0c373137ba7a24f33610
sorlros <sorlros2047@gmail.com>
2022-03-25 15:36:23 +0900

    vue3-syntax-caching

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <Fruits />
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
-import Fruits from '~/components/Fruits.vue'
 export default {
-  components: {
-    Fruits
+  data() {
+    return {
+      msg: "Hello Computed"
+    }
+  },
+  computed: { // computed 에 만들어놓은 계산된 데이터는 캐싱을 통해 한번 연산을 한 값이 있으면 또 다시 연산하지 않는다
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
   }
+  // },
+  // methods: {
+  //   reverseMessage() {
+  //     return this.msg.split('').reverse().join('')
+  //   }
+  // }
 }
 </script>
